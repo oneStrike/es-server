2a2930c56a19675121a22d4daac506820b51e2cf
lik <759994689@qq.com>
2025-11-17 11:06:50 +0800

    docs(dto): 完善DTO类的文档注释和属性定义
    为状态DTO、分页DTO和上传DTO添加详细的文档注释，并完善属性定义
    移除已废弃的PageResponseDto类

--- src/common/dto/page.dto.ts
+++ src/common/dto/page.dto.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger'
 import {
   ValidateJson,
   ValidateNumber,
@@ -28,7 +27,7 @@ export class PageDto {
   @ValidateJson({
     description: '排序字段，json格式',
     // prettier ignore
-    example: '{id:\'desc\'}',
+    example: "{id:'desc'}",
     required: false,
   })
   orderBy?: string
@@ -48,27 +47,4 @@ export class PageDto {
     type: 'ISO8601',
   })
   endDate?: string
-}
-
-export class PageResponseDto {
-  @ApiProperty({
-    description: '当前页码',
-    example: 0,
-    required: true,
-  })
-  pageIndex!: number
-
-  @ApiProperty({
-    description: '单页大小，最大500，默认15',
-    example: 0,
-    required: true,
-  })
-  pageSize!: number
-
-  @ApiProperty({
-    description: '符合条件的总记录数',
-    example: 0,
-    required: true,
-  })
-  total!: number
 }
--- src/common/dto/status.dto.ts
+++ src/common/dto/status.dto.ts
@@ -1,8 +1,14 @@
 import { ApiProperty, IntersectionType } from '@nestjs/swagger'
 import { IdDto, IdsDto, PublishDto, StatusDto } from './base.dto'
 
+/**
+ * 启用禁用
+ */
 export class EnabledDto extends IntersectionType(IdDto, StatusDto) {}
 
+/**
+ * 批量操作响应
+ */
 export class BatchOperationResponseDto {
   @ApiProperty({
     description: '操作成功的数据量',
@@ -11,7 +17,11 @@ export class BatchOperationResponseDto {
   })
   count!: number
 }
-
+/**
+ * 批量启用禁用
+ */
 export class BatchEnabledDto extends IntersectionType(IdsDto, StatusDto) {}
-
+/**
+ * 批量发布
+ */
 export class BatchPublishDto extends IntersectionType(IdsDto, PublishDto) {}
