9836c1be9b2010822b825d5153587eee44f841e6
lik <759994689@qq.com>
2025-11-27 09:06:10 +0800

    feat: 调整数据库种子文件地址

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from 'prisma/config'
 
 const seedCommand =
   process.env.NODE_ENV === 'production'
-    ? 'node dist/libs/database/seed/index.js'
+    ? 'node dist/libs/database/src/seed/index.js'
     : 'pnpm tsx src/prisma/seed/index.ts'
 
 export default defineConfig({
