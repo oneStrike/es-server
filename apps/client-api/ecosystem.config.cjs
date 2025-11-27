48755879e41dc7574102408eb840f8a519ae1396
lik <759994689@qq.com>
2025-11-27 09:40:15 +0800

    feat: 调整pm2运行

--- apps/client-api/ecosystem.config.cjs
+++ apps/client-api/ecosystem.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
   apps: [
     {
       name: 'client-api',
-      script: 'dist/apps/main.js',
+      script: 'dist/app/client-api/main.js',
       // Default one process per container; allow enabling cluster by env
       instances: process.env.PM2_INSTANCES || 1,
       exec_mode: process.env.PM2_EXEC_MODE || 'fork', // set 'cluster' to enable cluster mode
