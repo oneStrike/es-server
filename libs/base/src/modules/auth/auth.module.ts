2513a5b1c4607637c53075e37fad3016602cd811
lik <759994689@qq.com>
2025-12-04 14:28:56 +0800

    feat: 调整模块引用

--- libs/base/src/modules/auth/auth.module.ts
+++ libs/base/src/modules/auth/auth.module.ts
@@ -1,10 +1,10 @@
-import { JwtBlacklistService } from '@libs/base/modules'
 import { Global, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 import { AuthService } from './auth.service'
 import { AuthStrategy } from './auth.strategy'
+import { JwtBlacklistService } from './jwt-blacklist.service'
 
 @Global()
 @Module({
