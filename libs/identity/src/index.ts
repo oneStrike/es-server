1c52234d2c665a5e41ac2cbbb3cf3ddb687fb77a
lik <759994689@qq.com>
2026-03-24 13:51:28 +0800

    feat: 规范项目写法

--- libs/identity/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './identity.module'
-export * from './session.service'
-export * from './token/drizzle-token-storage.base'
--- libs/user/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './app-user-count.service'
-export * from './dto/base-app-user-count.dto'
-export * from './dto/base-app-user.dto'
-export * from './user.module'
-export * from './user.service'
